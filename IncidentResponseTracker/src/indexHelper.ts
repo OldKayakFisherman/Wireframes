619652a8d0af1dc562313b3dd3377ad12e57c64b
Rick Flagg <richard.lee.flagg@outlook.com>
2023-02-24 19:48:24 -0500

    Updated IRT wireframes

---
+++ IncidentResponseTracker/src/indexHelper.ts
@@ -0,0 +1,17 @@
+import {Chart} from 'chart.js'
+class IndexHelper
+{
+    public watch(){
+        
+    }
+}
+
+
+
+
+
+
+window.onload = () => {
+    let helper = new IndexHelper();
+    helper.watch();
+}
